c69ba2ed9e4f22941de54ef1b8cf927cc4d28dcb
Ansh Rawat <anshhh713@gmail.com>
2025-08-26 23:37:33 +0530

    feat: implement login and signup pages with styled components and routing

--- src/app/frontend/login/page.tsx
+++ src/app/frontend/login/page.tsx
@@ -1,18 +1,48 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Styles from './style.module.css';
 export default function LoginPage() {
-    return (
-        <>
-            <h1>Login</h1>
-            <form>
-                <label>
-                    Email:
-                    <input type="email" name="email" required />
-                </label>
-                <label>
-                    Password:
-                    <input type="password" name="password" required />
-                </label>
-                <button type="submit">Login</button>
-            </form>
-        </>
-    );
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+  const onLogin = async () => {};
+  return (
+    <>
+      <div className={Styles.login}>
+        <h1>Login</h1>
+        <form className={Styles.form}>
+          <label className={Styles.label}>
+            Email:
+            <input className={Styles.input}
+              type="email"
+              name="email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              placeholder="email"
+              required
+            />
+          </label>
+          <label className={Styles.label}>
+            Password:
+            <input className={Styles.input}
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              placeholder="password"
+              required
+            />
+          </label>
+          <button onClick={onLogin} type="submit">
+            Login
+          </button>
+          <p>
+            Don't have an account? <a href="/frontend/signup">Signup</a>
+          </p>
+        </form>
+      </div>
+    </>
+  );
 }
